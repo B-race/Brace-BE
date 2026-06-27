8af0dca10e1d5cadbe4c9414570e65cd246e49ff
mjttong <mjttong@gmail.com>
2026-06-28 00:07:53 +0900

    fix: application controller 미구현사항 구현

--- src/main/java/com/brace/server/application/controller/ApplicationController.java
+++ src/main/java/com/brace/server/application/controller/ApplicationController.java
@@ -6,44 +6,65 @@ import com.brace.server.application.dto.ApplicationResDto.ApplicationId;
 import com.brace.server.application.dto.ApplicationResDto.ApplicationResult;
 import com.brace.server.application.dto.ApplicationResDto.ApplicationSlice;
 import com.brace.server.application.dto.ApplicationResDto.ApplicationStatus;
+import com.brace.server.application.service.ApplicationService;
+import com.brace.server.global.apiPayload.ApiResponse;
+import com.brace.server.global.code.ApplicationSuccessCode;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RequiredArgsConstructor
 @RestController
 public class ApplicationController {
 
+    private final ApplicationService applicationService;
+
     @PostMapping("/projects/{projectId}/applications")
-    public ResponseEntity<ApplicationId> createApplication(
+    public ResponseEntity<ApiResponse<ApplicationId>> createApplication(
             @PathVariable Long projectId,
             @Valid @RequestBody Message request
     ) {
-        return null;
+        ApplicationId response = applicationService.createApplication(projectId, request);
+        return ResponseEntity
+                .status(ApplicationSuccessCode.CREATED.getHttpStatus())
+                .body(ApiResponse.success(ApplicationSuccessCode.CREATED, response));
     }
 
     @GetMapping("/projects/{projectId}/applications")
-    public ResponseEntity<ApplicationSlice> getApplications(
-            @PathVariable Long projectId,
-            @RequestParam(required = false) Long cursorId,
-            @RequestParam(defaultValue = "20") Integer size
+    public ResponseEntity<ApiResponse<ApplicationSlice>> getApplications(
+            @Positive @PathVariable Long projectId,
+            @Positive @RequestParam(required = false) Long cursorId,
+            @Min(1) @RequestParam(defaultValue = "20") Integer size
     ) {
-        return null;
+        ApplicationSlice response = applicationService.getApplications(projectId, cursorId, size);
+        return ResponseEntity
+                .status(ApplicationSuccessCode.READ_APPLICATIONS.getHttpStatus())
+                .body(ApiResponse.success(ApplicationSuccessCode.READ_APPLICATIONS, response));
     }
 
     @DeleteMapping("/applications/{applicationId}")
-    public ResponseEntity<ApplicationStatus> cancelApplication(
+    public ResponseEntity<ApiResponse<ApplicationStatus>> cancelApplication(
             @PathVariable Long applicationId
     ) {
-        return null;
+        ApplicationStatus response = applicationService.cancelApplication(applicationId);
+        return ResponseEntity
+                .status(ApplicationSuccessCode.CANCELED.getHttpStatus())
+                .body(ApiResponse.success(ApplicationSuccessCode.CANCELED, response));
     }
 
     @PatchMapping("/applications/{applicationId}")
-    public ResponseEntity<ApplicationResult> updateApplication(
+    public ResponseEntity<ApiResponse<ApplicationResult>> updateApplication(
             @PathVariable Long applicationId,
             @Valid @RequestBody Status request
     ) {
-        return null;
+        ApplicationResult response = applicationService.updateApplication(applicationId, request);
+        return ResponseEntity
+                .status(ApplicationSuccessCode.UPDATED.getHttpStatus())
+                .body(ApiResponse.success(ApplicationSuccessCode.UPDATED, response));
     }
 }
