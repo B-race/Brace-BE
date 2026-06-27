6b92a53938a4421db5646f0f60efb2ce6a39bd7d
lclee0520 <lc_lee0520@naver.com>
2026-06-27 21:25:15 +0900

    feat: 프로젝트 Response DTO 추가
    - ProjectSummaryResponse, ProjectDetailResponse, ProjectCreateResponse
    - RecruitmentStatusResponse, PageResponse

---
+++ src/main/java/com/brace/server/project/dto/response/ProjectCreateResponse.java
@@ -0,0 +1,10 @@
+package com.brace.server.project.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ProjectCreateResponse {
+    private Long projectId;
+}
